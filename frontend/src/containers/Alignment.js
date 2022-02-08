3d85040067367f71e55ab34e2bca81273b484dea
Arex360 <49303647+Arex360@users.noreply.github.com>
2022-02-08 00:06:14 -0800

    alignment settings

--- frontend/src/containers/Alignment.js
+++ frontend/src/containers/Alignment.js
@@ -4,15 +4,43 @@ import { setAlignment } from '../redux/actions/newVideoAction';
 const Alignment = () => {
 	const videoState = useSelector(state => state.video);
 	const [active, setActive] = useState(videoState.alignment ? videoState.alignment : 'center');
+	const [left,setLeft] = useState(false)
+	const [mid,setMid] = useState(false)
+	const [right,setRight] = useState(false)
+	const selectedStyle ={
+		background: '#ebf2ff',
+		border: '1px solid #3860ad',
+		color: '#3860ad'
+	}
+	let Reset =()=>{
+		setLeft(false)
+		setRight(false)
+		setMid(false)
+	}
+	let selectAlignmentLeft = ()=>{
+		Reset()
+		setLeft(true)
+		setActive('left')
+	}
+	let selectAlignmentMid = ()=>{
+		Reset()
+		setMid(true)
+		setActive('center')
+	}
+	let selectAlignmentRight = ()=>{
+		Reset()
+		setRight(true)
+		setActive('right')
+	}
 	const dispatch = useDispatch();
 	useEffect(() => {
 		dispatch(setAlignment(active))
 	}, [active])
 	return (
 		<div className="alignment">
-			<button onClick={() => setActive('left')} className="left">Left</button>
-			<button onClick={() => setActive('center')} className="center">Center</button>
-			<button onClick={() => setActive('right')} className="right">Right</button>
+			<button style={left ? selectedStyle:{}} onClick={selectAlignmentLeft} className="left">Left</button>
+			<button style={mid ? selectedStyle:{}} onClick={selectAlignmentMid} className="center">Center</button>
+			<button style={right ? selectedStyle:{}} onClick={selectAlignmentRight} className="right">Right</button>
 		</div>
 	)
 }
